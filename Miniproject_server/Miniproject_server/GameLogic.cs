956c5db03216ef7842d7fb987532b61e3659e4b1
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-09-23 15:56:56 +0200

    control op antwoorden

--- Miniproject_server/Miniproject_server/GameLogic.cs
+++ Miniproject_server/Miniproject_server/GameLogic.cs
@@ -8,5 +8,40 @@ namespace Miniproject_server
 {
     class GameLogic
     {
+        public GameLogic()
+        {
+
+        }
+
+        //color colorOption amount shape => pf3s = purple filled 3 swiggle
+        public Boolean CheckSet(string set1, string set2, string set3)
+        {
+            byte control = 0;
+            for (byte stringIndex = 0; stringIndex <= set1.Length; stringIndex++)
+            {
+                if (set1[stringIndex] == set2[stringIndex])
+                {
+                    if (set1[stringIndex] == set3[stringIndex])
+                    {
+                        control += 1;
+                    }
+                }
+                else if (set1[stringIndex] != set2[stringIndex])
+                {
+                    if (set1[stringIndex] != set3[stringIndex])
+                    {
+                        if (set2[stringIndex] != set3[stringIndex])
+                        {
+                            control += 1;
+                        }
+                    }
+                }
+            }
+            if (control == 4)
+                return true;
+            else return false;
+        }
+
+
     }
 }
