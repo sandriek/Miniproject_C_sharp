15cc629517b56fab3851e29e525f2a5d64042227
m <martijncaluwe@gmail.com>
2015-09-23 21:46:22 +0200

    plaatjes

--- miniproject/miniproject/Form1.Designer.cs
+++ miniproject/miniproject/Form1.Designer.cs
@@ -49,63 +49,71 @@
             this.x1x1.BackColor = System.Drawing.SystemColors.Control;
             this.x1x1.Enabled = false;
             this.x1x1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.x1x1.Location = new System.Drawing.Point(59, 87);
+            this.x1x1.Location = new System.Drawing.Point(44, 71);
+            this.x1x1.Margin = new System.Windows.Forms.Padding(2);
             this.x1x1.Name = "x1x1";
-            this.x1x1.Size = new System.Drawing.Size(146, 101);
+            this.x1x1.Size = new System.Drawing.Size(110, 82);
             this.x1x1.TabIndex = 0;
             this.x1x1.UseVisualStyleBackColor = false;
+            this.x1x1.Click += new System.EventHandler(this.x1x1_Click);
             // 
             // x1x2
             // 
             this.x1x2.BackColor = System.Drawing.SystemColors.Control;
-            this.x1x2.Location = new System.Drawing.Point(59, 198);
+            this.x1x2.Location = new System.Drawing.Point(44, 161);
+            this.x1x2.Margin = new System.Windows.Forms.Padding(2);
             this.x1x2.Name = "x1x2";
-            this.x1x2.Size = new System.Drawing.Size(146, 101);
+            this.x1x2.Size = new System.Drawing.Size(110, 82);
             this.x1x2.TabIndex = 0;
             this.x1x2.UseVisualStyleBackColor = false;
             // 
             // x1x3
             // 
             this.x1x3.BackColor = System.Drawing.SystemColors.Control;
-            this.x1x3.Location = new System.Drawing.Point(59, 305);
+            this.x1x3.Location = new System.Drawing.Point(44, 248);
+            this.x1x3.Margin = new System.Windows.Forms.Padding(2);
             this.x1x3.Name = "x1x3";
-            this.x1x3.Size = new System.Drawing.Size(146, 101);
+            this.x1x3.Size = new System.Drawing.Size(110, 82);
             this.x1x3.TabIndex = 0;
             this.x1x3.UseVisualStyleBackColor = false;
             // 
             // x2x3
             // 
             this.x2x3.BackColor = System.Drawing.SystemColors.Control;
-            this.x2x3.Location = new System.Drawing.Point(211, 305);
+            this.x2x3.Location = new System.Drawing.Point(158, 248);
+            this.x2x3.Margin = new System.Windows.Forms.Padding(2);
             this.x2x3.Name = "x2x3";
-            this.x2x3.Size = new System.Drawing.Size(146, 101);
+            this.x2x3.Size = new System.Drawing.Size(110, 82);
             this.x2x3.TabIndex = 0;
             this.x2x3.UseVisualStyleBackColor = false;
             // 
             // x3x3
             // 
             this.x3x3.BackColor = System.Drawing.SystemColors.Control;
-            this.x3x3.Location = new System.Drawing.Point(364, 305);
+            this.x3x3.Location = new System.Drawing.Point(273, 248);
+            this.x3x3.Margin = new System.Windows.Forms.Padding(2);
             this.x3x3.Name = "x3x3";
-            this.x3x3.Size = new System.Drawing.Size(146, 101);
+            this.x3x3.Size = new System.Drawing.Size(110, 82);
             this.x3x3.TabIndex = 0;
             this.x3x3.UseVisualStyleBackColor = false;
             // 
             // x3x2
             // 
             this.x3x2.BackColor = System.Drawing.SystemColors.Control;
-            this.x3x2.Location = new System.Drawing.Point(364, 198);
+            this.x3x2.Location = new System.Drawing.Point(273, 161);
+            this.x3x2.Margin = new System.Windows.Forms.Padding(2);
             this.x3x2.Name = "x3x2";
-            this.x3x2.Size = new System.Drawing.Size(146, 101);
+            this.x3x2.Size = new System.Drawing.Size(110, 82);
             this.x3x2.TabIndex = 0;
             this.x3x2.UseVisualStyleBackColor = false;
             // 
             // x2x2
             // 
             this.x2x2.BackColor = System.Drawing.SystemColors.Control;
-            this.x2x2.Location = new System.Drawing.Point(211, 198);
+            this.x2x2.Location = new System.Drawing.Point(158, 161);
+            this.x2x2.Margin = new System.Windows.Forms.Padding(2);
             this.x2x2.Name = "x2x2";
-            this.x2x2.Size = new System.Drawing.Size(146, 101);
+            this.x2x2.Size = new System.Drawing.Size(110, 82);
             this.x2x2.TabIndex = 0;
             this.x2x2.UseVisualStyleBackColor = false;
             // 
@@ -113,27 +121,31 @@
             // 
             this.x2x1.BackColor = System.Drawing.SystemColors.Control;
             this.x2x1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.x2x1.Location = new System.Drawing.Point(211, 87);
+            this.x2x1.Location = new System.Drawing.Point(158, 71);
+            this.x2x1.Margin = new System.Windows.Forms.Padding(2);
             this.x2x1.Name = "x2x1";
-            this.x2x1.Size = new System.Drawing.Size(146, 101);
+            this.x2x1.Size = new System.Drawing.Size(110, 82);
             this.x2x1.TabIndex = 0;
             this.x2x1.UseVisualStyleBackColor = false;
+            this.x2x1.Click += new System.EventHandler(this.x2x1_Click);
             // 
             // x3x1
             // 
             this.x3x1.BackColor = System.Drawing.SystemColors.Control;
-            this.x3x1.Location = new System.Drawing.Point(363, 87);
+            this.x3x1.Location = new System.Drawing.Point(272, 71);
+            this.x3x1.Margin = new System.Windows.Forms.Padding(2);
             this.x3x1.Name = "x3x1";
-            this.x3x1.Size = new System.Drawing.Size(146, 101);
+            this.x3x1.Size = new System.Drawing.Size(110, 82);
             this.x3x1.TabIndex = 0;
             this.x3x1.UseVisualStyleBackColor = false;
             // 
             // x4x3
             // 
             this.x4x3.BackColor = System.Drawing.SystemColors.Control;
-            this.x4x3.Location = new System.Drawing.Point(515, 305);
+            this.x4x3.Location = new System.Drawing.Point(386, 248);
+            this.x4x3.Margin = new System.Windows.Forms.Padding(2);
             this.x4x3.Name = "x4x3";
-            this.x4x3.Size = new System.Drawing.Size(146, 101);
+            this.x4x3.Size = new System.Drawing.Size(110, 82);
             this.x4x3.TabIndex = 0;
             this.x4x3.Text = "\\";
             this.x4x3.UseVisualStyleBackColor = false;
@@ -141,26 +153,29 @@
             // x4x2
             // 
             this.x4x2.BackColor = System.Drawing.SystemColors.Control;
-            this.x4x2.Location = new System.Drawing.Point(516, 198);
+            this.x4x2.Location = new System.Drawing.Point(387, 161);
+            this.x4x2.Margin = new System.Windows.Forms.Padding(2);
             this.x4x2.Name = "x4x2";
-            this.x4x2.Size = new System.Drawing.Size(146, 101);
+            this.x4x2.Size = new System.Drawing.Size(110, 82);
             this.x4x2.TabIndex = 0;
             this.x4x2.UseVisualStyleBackColor = false;
             // 
             // x4x1
             // 
             this.x4x1.BackColor = System.Drawing.SystemColors.Control;
-            this.x4x1.Location = new System.Drawing.Point(515, 87);
+            this.x4x1.Location = new System.Drawing.Point(386, 71);
+            this.x4x1.Margin = new System.Windows.Forms.Padding(2);
             this.x4x1.Name = "x4x1";
-            this.x4x1.Size = new System.Drawing.Size(146, 101);
+            this.x4x1.Size = new System.Drawing.Size(110, 82);
             this.x4x1.TabIndex = 0;
             this.x4x1.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 435);
+            this.button1.Location = new System.Drawing.Point(224, 353);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 42);
+            this.button1.Size = new System.Drawing.Size(100, 34);
             this.button1.TabIndex = 1;
             this.button1.Text = "connect";
             this.button1.UseVisualStyleBackColor = true;
@@ -169,17 +184,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(337, 35);
+            this.label1.Location = new System.Drawing.Point(253, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 506);
+            this.ClientSize = new System.Drawing.Size(549, 411);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.x4x1);
@@ -194,6 +210,7 @@
             this.Controls.Add(this.x1x3);
             this.Controls.Add(this.x1x2);
             this.Controls.Add(this.x1x1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- miniproject/miniproject/Form1.cs
+++ miniproject/miniproject/Form1.cs
@@ -12,9 +12,92 @@ using System.Net;
 
 namespace miniproject
 {
-
     public partial class Form1 : Form
     {
+        Image groenstreepkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep3.gif");
+        Image groenrechtstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep3r.gif");
+        Image groenvliegerstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerstreep2.gif");
+        Image groenrechtstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep2r.gif");
+        Image groenstreepkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep2.gif");
+        Image groenvliegerstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerstreep2.gif");
+        Image groenstreepkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep1.gif");
+        Image groenrechtstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenstreep1r.gif");
+        Image groenvliegerstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerstreep1.gif");
+        Image groenheelkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel3r.gif");
+        Image groenrechtheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel3.gif");
+        Image groenvliegerheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerheel3.gif");
+        Image groenheelkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel2.gif");
+        Image groenrechtheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel2r.gif");
+        Image groenvliegerheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerheel2.gif");
+        Image groenrechtheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel1r.gif");
+        Image groenheelkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenheel1.gif");
+        Image groenvliegerheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerheel1.gif");
+        Image groenleegkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg3.gif");
+        Image groenrechtleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg3r.gif");
+        Image groenvliegerleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerleeg3.gif");
+        Image groenrechtleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg2r.gif");
+        Image groenleegkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg2.gif");
+        Image groenvliegerleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerleeg2.gif");
+        Image groenleegkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg1.gif");
+        Image groenrechtleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenleeg1r.gif");
+        Image groenvliegerleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Groenvliegerleeg1.gif");
+
+        Image roodstreepkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep3.gif");
+        Image roodrechtstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep3r.gif");
+        Image roodvliegerstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerstreep2.gif");
+        Image roodrechtstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep2r.gif");
+        Image roodstreepkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep2.gif");
+        Image roodvliegerstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerstreep2.gif");
+        Image roodstreepkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep1.gif");
+        Image roodrechtstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodstreep1r.gif");
+        Image roodvliegerstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerstreep1.gif");
+        Image roodheelkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel3r.gif");
+        Image roodrechtheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel3.gif");
+        Image roodvliegerheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerheel3.gif");
+        Image roodheelkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel2.gif");
+        Image roodrechtheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel2r.gif");
+        Image roodvliegerheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerheel2.gif");
+        Image roodrechtheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel1r.gif");
+        Image roodheelkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodheel1.gif");
+        Image roodvliegerheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerheel1.gif");
+        Image roodleegkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg3.gif");
+        Image roodrechtleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg3r.gif");
+        Image roodvliegerleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerleeg3.gif");
+        Image roodrechtleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg2r.gif");
+        Image roodleegkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg2.gif");
+        Image roodvliegerleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerleeg2.gif");
+        Image roodleegkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg1.gif");
+        Image roodrechtleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodleeg1r.gif");
+        Image roodvliegerleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Roodvliegerleeg1.gif");
+
+        Image paarsstreepkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep3.gif");
+        Image paarsrechtstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep3r.gif");
+        Image paarsvliegerstreep3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerstreep2.gif");
+        Image paarsrechtstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep2r.gif");
+        Image paarsstreepkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep2.gif");
+        Image paarsvliegerstreep2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerstreep2.gif");
+        Image paarsstreepkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep1.gif");
+        Image paarsrechtstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsstreep1r.gif");
+        Image paarsvliegerstreep1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerstreep1.gif");
+        Image paarsheelkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel3r.gif");
+        Image paarsrechtheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel3.gif");
+        Image paarsvliegerheel3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerheel3.gif");
+        Image paarsheelkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel2.gif");
+        Image paarsrechtheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel2r.gif");
+        Image paarsvliegerheel2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerheel2.gif");
+        Image paarsrechtheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel1r.gif");
+        Image paarsheelkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsheel1.gif");
+        Image paarsvliegerheel1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerheel1.gif");
+        Image paarsleegkrom3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg3.gif");
+        Image paarsrechtleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg3r.gif");
+        Image paarsvliegerleeg3 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerleeg3.gif");
+        Image paarsrechtleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg2r.gif");
+        Image paarsleegkrom2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg2.gif");
+        Image paarsvliegerleeg2 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerleeg2.gif");
+        Image paarsleegkrom1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg1.gif");
+        Image paarsrechtleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsleeg1r.gif");
+        Image paarsvliegerleeg1 = Image.FromFile("C:\\Users\\martijn\\Documents\\GitHub\\Miniproject_C_sharp\\miniproject\\Pictures\\Paarsvliegerleeg1.gif");
+
         System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
         public Form1()
         {
@@ -31,11 +114,6 @@ namespace miniproject
 
             clientSocket.Connect("145.48.231.46", 8888);
             WriteMessage(clientSocket, "derp");
-00
-
-            
-
-
             
         }
         private static void WriteMessage(TcpClient client, string message)
@@ -47,6 +125,18 @@ namespace miniproject
             client.GetStream().Write(bytes, 0, bytes.Length);
         }
 
+
+
+        private void x1x1_Click(object sender, EventArgs e)
+        {
+            //Button.image
+            ///x1x1.Image = Resources
+        }
+
+        private void x2x1_Click(object sender, EventArgs e)
+        {
+            x2x1.Image = roodheelkrom1;
+        }
     }
     }
 
